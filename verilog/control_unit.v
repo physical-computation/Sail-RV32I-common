dd96fe5821469127fdb6493e1d72254b387d67b2
Ryan Voo <ryanvoo95@gmail.com>
2019-05-19 13:08:48 +0100

    Modified control signals

--- verilog/control_unit.v
+++ verilog/control_unit.v
@@ -58,16 +58,17 @@ module control(
 	input	[6:0] opcode;
 	output	MemtoReg, RegWrite, MemWrite, MemRead, Branch, ALUSrc, Jump, Jalr, Lui, Auipc, Fence, CSRR;
 
-	assign MemtoReg = (~opcode[5]) & (~opcode[4]) & (~opcode[3]) & (opcode[0]);
-	assign RegWrite = ((~(opcode[4] | opcode[5])) | opcode[2] | opcode[4]) & opcode[0];
-	assign MemWrite = (~opcode[6]) & (opcode[5]) & (~opcode[4]);
-	assign MemRead = (~opcode[5]) & (~opcode[4]) & (~opcode[3]) & (opcode[1]);
+	assign MemtoReg = (~opcode[5]) & (~opcode[4]) & (~opcode[3]);
+	assign RegWrite = ((~opcode[5]) & (~opcode[3])) | (opcode[4] & (~opcode[3])) | opcode[2];
+	assign MemWrite = (~opcode[6]) & (opcode[5]) & (~opcode[4]) & (~opcode[3]);
+	assign MemRead = (~opcode[5]) & (~opcode[4]) & (~opcode[3]);
 	assign Branch = (opcode[6]) & (~opcode[4]) & (~opcode[2]);
-	assign ALUSrc = ~(opcode[6] | opcode[4]) | (~opcode[5]);
-	assign Jump = (opcode[6]) & (opcode[5]) & (~opcode[4]) & (opcode[2]);
-	assign Jalr = (opcode[6]) & (opcode[5]) & (~opcode[4]) & (~opcode[3]) & (opcode[2]);
-	assign Lui = (~opcode[6]) & (opcode[5]) & (opcode[4]) & (~opcode[3]) & (opcode[2]);
-	assign Auipc = (~opcode[6]) & (~opcode[5]) & (opcode[4]) & (~opcode[3]) & (opcode[2]);
-	assign Fence = (~opcode[5]) & opcode[3] & (opcode[2]);
-	assign CSRR = (opcode[6]) & (opcode[4]);
+	assign ALUSrc = ((~opcode[6]) & (~opcode[5]) & opcode[2]) + ((~opcode[6]) & (~opcode[4]) & (~opcode[3])) + ((~opcode[6]) & (~opcode[5]) & opcode[4]);
+	assign Jump = (opcode[6]) & (opcode[2]);
+	assign Jalr = (~opcode[4]) & (~opcode[3]) & (opcode[2]);
+	assign Lui = (~opcode[6]) & (opcode[5]) & (opcode[2]);
+	assign Auipc = (~opcode[5]) & (opcode[4]) & (opcode[2]);
+	assign Fence = (~opcode[5]) & (~opcode[4]) & (opcode[2]);
+	assign CSRR = (opcode[6]) & (opcode[4]) & (~opcode[3]);
+	
 endmodule
