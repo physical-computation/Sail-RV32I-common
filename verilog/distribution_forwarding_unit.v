ce41d9aa8a66d97b5c3f0d8e84228d8c6bee2b1a
Ryan Voo <ryanvoo95@gmail.com>
2019-05-21 17:49:44 +0100

    Added interface to forward operands from DRs2, in distribution forwarding unit

--- verilog/distribution_forwarding_unit.v
+++ verilog/distribution_forwarding_unit.v
@@ -1,6 +1,7 @@
 
 module DistributionForwardingUnit (
-		input[4:0] ID_sourceAddr,
+		input[4:0] ID_sourceAddr1,
+		input[4:0] ID_sourceAddr2,
 		
 		input EX_DRegWrite,
 		input[4:0] EX_destRegAddr,
@@ -8,11 +9,17 @@ module DistributionForwardingUnit (
 		input MEM_DRegWrite,
 		input[4:0] MEM_destRegAddr,
 		
-		output EX_DFwdMuxSel,
-		output MEM_DFwdMuxSel
+		output EX_DFwdMuxSel1,
+		output MEM_DFwdMuxSel1,
+		
+		output EX_DFwdMuxSel2,
+		output MEM_DFwdMuxSel2
 	);
 	
-	assign EX_DFwdMuxSel = ((ID_sourceAddr == EX_destRegAddr) && EX_DRegWrite);
-	assign MEM_DFwdMuxSel = ((EX_destRegAddr != MEM_destRegAddr) && (ID_sourceAddr == MEM_destRegAddr) && MEM_DRegWrite);
+	assign EX_DFwdMuxSel1 = ((ID_sourceAddr1 == EX_destRegAddr) && EX_DRegWrite);
+	assign MEM_DFwdMuxSel1 = ((EX_destRegAddr != MEM_destRegAddr) && (ID_sourceAddr1 == MEM_destRegAddr) && MEM_DRegWrite);
+	
+	assign EX_DFwdMuxSel2 = ((ID_sourceAddr2 == EX_destRegAddr) && EX_DRegWrite);
+	assign MEM_DFwdMuxSel2 = ((EX_destRegAddr != MEM_destRegAddr) && (ID_sourceAddr2 == MEM_destRegAddr) && MEM_DRegWrite);
 	
 endmodule
