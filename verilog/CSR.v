494658ebd338bf9b2db6d3aefe1c3bc3f9e11f8b
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2019-05-23 20:45:29 +0100

    cleanup

--- verilog/CSR.v
+++ verilog/CSR.v
@@ -54,8 +54,6 @@ module csr_file (clk, write, wrAddr_CSR, wrVal_CSR, rdAddr_CSR, rdVal_CSR);
 
 	reg [31:0] csr_file [0:2**10-1];
 
-	//assign rdVal_CSR = ((wrAddr_CSR==rdAddr_CSR) & write) ? wrVal_CSR : csr_file[rdAddr_CSR];
-
 	always @(posedge clk) begin
 		if (write) begin
 			csr_file[wrAddr_CSR] <= wrVal_CSR;
@@ -63,22 +61,4 @@ module csr_file (clk, write, wrAddr_CSR, wrVal_CSR, rdAddr_CSR, rdVal_CSR);
 		rdVal_CSR <= csr_file[rdAddr_CSR];
 	end
 
-
-	/*
-	SB_RAM40_4K endValueSPRAM (
-		.RDATA(endValueRDATA),
-		.RADDR(blockRamAddr),
-		.RCLK(clk),
-		.RCLKE(endValueRCLKE),
-		.RE(endValueRE),
-		.WADDR(blockRamAddr),
-		.WCLK(clk),
-		.WCLKE(endValueWCLKE),
-		.WDATA(endValueWDATA),
-		.WE(endValueWE),
-		.MASK(blockRamMask)
-	);
-	defparam endValueSPRAM.READ_MODE=0;
-	defparam endValueSPRAM.WRITE_MODE=0;
-	*/
 endmodule
